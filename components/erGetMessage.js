071c9ebab37ec6d1de5b8d955bfce49d0f5e7b53
liuxiong332 <liuxiong332@163.com>
2014-09-22 17:54:34 +0800

    add the erGetMessage.js

--- components/erGetMessage.js
+++ components/erGetMessage.js
@@ -62,14 +62,10 @@ function erGetMessageRequest(aArgument, aCbOk, aCbError, aListener)
 		function(aExchangeRequest, aResp) { self.onSendOk(aExchangeRequest, aResp);},
 		function(aExchangeRequest, aCode, aMsg) { self.onSendError(aExchangeRequest, aCode, aMsg);},
 		aListener);
-
 	this.argument = aArgument;
 	this.serverUrl = aArgument.serverUrl;
 	this.listener = aListener;
-	this.ids = aArgument.ids;
-	this.counter = 0;
-
-	this.folderClass = aArgument.folderClass;
+	this.messages = aArgument.messages;
 
 	this.isRunning = true;
 	this.requestedItemId = [];
@@ -87,197 +83,22 @@ erGetMessageRequest.prototype = {
 		var req = root[telements][0];
 
 		var itemShape = xml2json.addTag(req, "ItemShape", "nsMessages", null);
-		xml2json.addTag(itemShape, "BaseShape", "nsTypes", "IdOnly");
-//		xml2json.addTag(itemShape, "BodyType", "nsTypes", "Text");
-		xml2json.addTag(itemShape, "BodyType", "nsTypes", "Best");
-
-		var additionalProperties = xml2json.addTag(itemShape, "AdditionalProperties", "nsTypes", null);
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:ItemId'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:ParentFolderId'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:ItemClass'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:Attachments'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:Subject'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:DateTimeReceived'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:Size'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:Categories'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:HasAttachments'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:Importance'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:IsDraft'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:IsFromMe'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:IsResend'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:IsSubmitted'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:IsUnmodified'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:DateTimeSent'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:DateTimeCreated'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:Body'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:ResponseObjects'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:Sensitivity'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:ReminderDueBy'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:ReminderIsSet'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:ReminderMinutesBeforeStart'/>");
-		xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:EffectiveRights'/>");
-		//xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:MimeContent'/>");
-
-		this.exchangeStatistics = Cc["@1st-setup.nl/exchange/statistics;1"]
-				.getService(Ci.mivExchangeStatistics);
-
-		if ((this.exchangeStatistics.getServerVersion(this.serverUrl).indexOf("Exchange2010") > -1) || (this.exchangeStatistics.getServerVersion(this.serverUrl).indexOf("Exchange2013") > -1 )) {
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='item:UniqueBody'/>");
-		}
-		else { // Exchange2007
-		}
-
-		var extFieldURI;
-		extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-		xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Common");
-		xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidReminderSignalTime);
-		xml2json.setAttribute(extFieldURI, "PropertyType", "SystemTime");
-
-		extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-		xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Common");
-		xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidReminderSet);
-		xml2json.setAttribute(extFieldURI, "PropertyType", "Boolean");
-
-		extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-		xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Common");
-		xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidReminderDelta);
-		xml2json.setAttribute(extFieldURI, "PropertyType", "Integer");
-
-			// Calendar fields
-		switch (this.folderClass) {
-		case "IPF.Appointment":
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:Start'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:End'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:OriginalStart'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:IsAllDayEvent'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:LegacyFreeBusyStatus'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:Location'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:When'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:IsMeeting'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:IsCancelled'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:IsRecurring'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:MeetingRequestWasSent'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:IsResponseRequested'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:CalendarItemType'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:MyResponseType'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:Organizer'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:RequiredAttendees'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:OptionalAttendees'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:Resources'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:Duration'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:TimeZone'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:Recurrence'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:ConferenceType'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:AllowNewTimeProposal'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:IsOnlineMeeting'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:MeetingWorkspaceUrl'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:UID'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:RecurrenceId'/>");
-
-			if ((this.exchangeStatistics.getServerVersion(this.serverUrl).indexOf("Exchange2010") > -1) || (this.exchangeStatistics.getServerVersion(this.serverUrl).indexOf("Exchange2013") > -1 )) {
-				xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:StartTimeZone'/>");
-				xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:EndTimeZone'/>");
-				xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:ModifiedOccurrences'/>");
-				xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:DeletedOccurrences'/>");
-				xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:FirstOccurrence'/>");
-				xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:LastOccurrence'/>");
-			}
-			else { // Exchange2007
-				xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='calendar:MeetingTimeZone'/>");
-			}
-			break;
-
-		case "IPF.Task":
-			//Task fields
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:ActualWork'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:AssignedTime'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:BillingInformation'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:ChangeCount'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:Companies'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:CompleteDate'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:Contacts'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:DelegationState'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:Delegator'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:DueDate'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:IsAssignmentEditable'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:IsComplete'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:IsRecurring'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:IsTeamTask'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:Mileage'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:Owner'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:PercentComplete'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:Recurrence'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:StartDate'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:Status'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:StatusDescription'/>");
-			xml2json.parseXML(additionalProperties,"<nsTypes:FieldURI FieldURI='task:TotalWork'/>");
-
-			extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-			xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Task");
-			xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidTaskAccepted);
-			xml2json.setAttribute(extFieldURI, "PropertyType", "Boolean");
-
-			extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-			xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Task");
-			xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidTaskLastUpdate);
-			xml2json.setAttribute(extFieldURI, "PropertyType", "SystemTime");
-
-			extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-			xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Task");
-			xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidTaskAcceptanceState);
-			xml2json.setAttribute(extFieldURI, "PropertyType", "Integer");
-
-			extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-			xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Task");
-			xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidTaskMode);
-			xml2json.setAttribute(extFieldURI, "PropertyType", "Integer");
-
-			extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-			xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Task");
-			xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidTaskGlobalId);
-			xml2json.setAttribute(extFieldURI, "PropertyType", "Binary");
-
-			extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-			xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Task");
-			xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidTaskHistory);
-			xml2json.setAttribute(extFieldURI, "PropertyType", "Integer");
-
-			extFieldURI = xml2json.addTag(additionalProperties, "ExtendedFieldURI", "nsTypes", null);
-			xml2json.setAttribute(extFieldURI, "DistinguishedPropertySetId", "Task");
-			xml2json.setAttribute(extFieldURI, "PropertyId", MAPI_PidLidTaskOwnership);
-			xml2json.setAttribute(extFieldURI, "PropertyType", "Integer");
-		}
-/*
-			//meeting fields
-			req.nsMessages::ItemShape.nsTypes::AdditionalProperties.content += <>
-				<nsTypes:FieldURI FieldURI="meeting:AssociatedCalendarItemId" xmlns:nsTypes={nsTypes}/>
-				<nsTypes:FieldURI FieldURI="meeting:IsDelegated" xmlns:nsTypes={nsTypes}/>
-				<nsTypes:FieldURI FieldURI="meeting:IsOutOfDate" xmlns:nsTypes={nsTypes}/>
-				<nsTypes:FieldURI FieldURI="meeting:HasBeenProcessed" xmlns:nsTypes={nsTypes}/>
-				<nsTypes:FieldURI FieldURI="meeting:ResponseType" xmlns:nsTypes={nsTypes}/>
-				<nsTypes:FieldURI FieldURI="meetingRequest:MeetingRequestType" xmlns:nsTypes={nsTypes}/>
-				<nsTypes:FieldURI FieldURI="meetingRequest:IntendedFreeBusyStatus" xmlns:nsTypes={nsTypes}/>
-				</>;
-*/
+		xml2json.addTag(itemShape, "BaseShape", "nsTypes", "Default");
+		xml2json.addTag(itemShape, 'IncludeMimeContent', 'nsTypes', 'true');
 
 		var itemids = xml2json.addTag(req, "ItemIds", "nsMessages", null);
-		for each (var item in this.ids) {
+		for each (var item in this.messages) {
 			var itemId = xml2json.addTag(itemids, "ItemId", "nsTypes", null);
 			xml2json.setAttribute(itemId, "Id", item.Id);
 			this.requestedItemId.push(item.Id);
 			if (item.ChangeKey) {
 				xml2json.setAttribute(itemId, "ChangeKey", item.ChangeKey);
 			}
-			if (item.index) {
-				//exchWebService.commonFunctions.LOG("erGetTaskItemsRequest.execute. We have an index.");
-				this.argument.occurrenceIndexes[item.Id] = item.index;
-			}
 			itemId = null;
 		}
 		itemids = null;
 
 		this.parent.xml2json = true;
-
 		//dump("erGetItemsRequest.execute:"+String(this.parent.makeSoapMessage2(req))+"\n");
 
 		this.parent.sendRequest(this.parent.makeSoapMessage2(req), this.serverUrl);
@@ -292,6 +113,7 @@ erGetMessageRequest.prototype = {
 		//dump("erGetItemsRequest.onSendOk: "+xml2json.toString(aResp)+"\n");
 		var rm = xml2json.XPath(aResp, "/s:Envelope/s:Body/m:GetItemResponse/m:ResponseMessages/m:GetItemResponseMessage[@ResponseClass='Success' and m:ResponseCode='NoError']/m:Items/*");
 
+		exchWebService.commonFunctions.LOG('the get message response is ' + aResp);
 		var rmErrorSearch = xml2json.XPath(aResp, "/s:Envelope/s:Body/m:GetItemResponse/m:ResponseMessages/m:GetItemResponseMessage");
 		var rmErrors = [];
 		if (rmErrorSearch.length > 0) {
--- test/components/test_erFindMessages.js
+++ test/components/test_erFindMessages.js
@@ -1,21 +1,22 @@
 
-QUnit.module('erFindMessages test', {
+QUnit.module('erFindMessages and erGetMessage test', {
   setup: function() {
     QUnit.Cc = Components.classes;
     QUnit.Ci = Components.interfaces;
     QUnit.Cu = Components.utils;
     QUnit.Cu.import('resource://exchangeEws/commonFunctions.js', QUnit);
     QUnit.Cu.import('resource://exchangeEws/erFindMessages.js', QUnit);
+    QUnit.Cu.import('resource://exchangeEws/erGetMessage.js', QUnit)
     QUnit.baseLog = QUnit.commonFunctions.baseLog;
   },
   teardown: function() {
+    delete QUnit.erGetMessageRequest;
     delete QUnit.erFindMessagesRequest;
     delete QUnit.commonFunctions;
   }
 });
 
 QUnit.asyncTest('request messages find', function(assert) {
-  expect(1);
   var requestConfig = {
     maxReturned: 10,
     serverUrl: 'https://bjmail.kingsoft.com/EWS/exchange.asmx',
@@ -24,10 +25,27 @@ QUnit.asyncTest('request messages find', function(assert) {
     password: 'abcd.ABCD'
   };
 
+  function getMessages(messages) {
+    function msgRequestOK(request, msgInfos) {
+      assert.ok(true, 'get the messages');
+      QUnit.start();
+    }
+    function msgRequestError(request, code, msg) {
+      QUnit.baseLog.info('the response code is' + code + " message is " + msg);
+      assert.ok(false, 'request failed!');
+      QUnit.start();
+    }
+
+    requestConfig.messages = messages.slice(0, 1);
+
+    var messageRequest = new QUnit.erGetMessageRequest(requestConfig,
+      msgRequestOK, msgRequestError);
+  }
+
   function requestOK(request, messages) {
     assert.ok(true, 'the find messages request ok!');
     QUnit.baseLog.info(QUnit.dump.parse(messages));
-    QUnit.start();
+    getMessages(messages);
   }
   function requestError(request, code, msg) {
     QUnit.baseLog.info('the response code is' + code + " message is " + msg);
